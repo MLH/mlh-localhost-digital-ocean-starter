10721f2d98ba0570911b43d96c32c1bfc545f731
esouthren <eilidhsouthren@gmail.com>
2019-09-01 12:05:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-/* Add code here! */
+const express = require('express');
+const app = express();
+const port = 3000;
+const request = require('request')
+const fs = require("fs");
+
+app.use(express.static('public'));
+
+const path = require('path');
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+})
+
+app.listen(port, () => console.log(`Digital Ocean app listening on port ${port}!`))
